677300f58011dff3a63b938f8621d6c379334264
mpmaggi <marcus.maggi@minutrade.com>
2017-11-27 03:01:40 -0200

    loaderio

--- external/routes.js
+++ external/routes.js
@@ -2,4 +2,5 @@ const { codegen } = require('./controller');
 
 module.exports = (app) => {
   app.post('/pincode/', codegen);
+  app.get('/loaderio-e8eea5d3c4c7426094c9c870a318f234', (req, res) => { res.status(200).send('loaderio-e8eea5d3c4c7426094c9c870a318f234') });
 };
